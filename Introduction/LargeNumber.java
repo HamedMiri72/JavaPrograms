c5bc92e9c9f7850a76cfaa8ebf044f99086240fa
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-26 22:20:03 +0000

    Large number

---
+++ Introduction/LargeNumber.java
@@ -0,0 +1,17 @@
+public class LargeNumber {
+
+    public static void main(String[] args) {
+        int first = 5;
+    int second = 7;
+    int thierd = 10;
+
+    if (first >= second && first >= thierd){
+        System.out.println(first + " is the grater number");   
+    }else if(second >= first && second >= thierd){
+        System.out.println(thierd + " is a grater number!");
+    }else{
+        System.out.println(second + " is a grater number!");
+    }
+    
+    }
+}
