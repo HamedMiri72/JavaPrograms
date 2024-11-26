1f3fab128ee8fb68314b8325ae7e99afff9fcab4
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-26 15:51:34 +0000

    Add two integer

---
+++ Introduction/AddInteger.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class AddInteger {
+    public static void main(String[] args) {
+     Scanner reader = new Scanner(System.in);
+     System.out.println("Enter first number: ");
+     int first = reader.nextInt();
+     System.out.println("Enter second number: ");
+     int second = reader.nextInt();
+     int sum = first + second;
+
+     System.out.println("Sum is: " + sum);
+        
+    }
+    
+}
