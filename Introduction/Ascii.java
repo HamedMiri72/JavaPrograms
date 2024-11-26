ea6e94c05c69ee44f2582cff3a152db7483741c6
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-26 20:48:11 +0000

    ASCII

---
+++ Introduction/Ascii.java
@@ -0,0 +1,15 @@
+
+//American Standard Code for Information Interchange
+
+public class Ascii {
+    public static void main(String[] arg){
+
+        char ch = 'b';
+        int ascii = ch;
+        int castascii = (int) ch;
+        System.out.println("the ASCII value of :" + ascii);
+        System.out.println("the ASCII value of " + ch + "is: " + castAscii);
+
+    }
+    
+}
