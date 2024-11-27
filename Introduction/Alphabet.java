9879653b091b650f53bd4de8261efe5dd63e43fd
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-27 12:40:32 +0000

    Alphabet

---
+++ Introduction/Alphabet.java
@@ -0,0 +1,16 @@
+
+
+public class Alphabet {
+    
+    public static void main(String[] args){
+
+        char ch = 't';
+        //char basicllay store the ascii value of the ch in the variable
+        if( ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z'){
+            System.out.println(ch +"is an alphabet");
+        } else {
+            System.out.println("it is not an alphabt");
+        }
+    }
+
+}
