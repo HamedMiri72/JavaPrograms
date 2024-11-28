6353d54899ec4c971815c5af7e5bacc0036fb938
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-28 19:38:31 +0000

    Round Decimal

--- Introduction/Round.java
+++ Introduction/Round.java
@@ -1,8 +1,16 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Round {
 
     public static void main(String[] args){
         double num = 1.3464647;
         System.out.format("%.2f", num);
+
+        double number = 4.3834382;
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        System.out.println(df.format(number));
     }
     
 }
