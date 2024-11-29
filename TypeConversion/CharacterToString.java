61a5f227e28a16a90f0ec15338574d17d8ba7942
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-29 21:41:37 +0000

    characterTo String

---
+++ TypeConversion/CharacterToString.java
@@ -0,0 +1,14 @@
+
+public class CharacterToString {
+    public static void main(String[] args){
+
+        char ch = 'c';
+        String str = Character.toString(ch);
+
+        //str = String.valueOf(ch);
+
+        System.out.println("the string is :" + str);
+    }
+    
+}
+
