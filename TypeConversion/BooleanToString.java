8cce21f852364e6280f7da7dd5a21729aaed3b3c
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-29 22:12:26 +0000

    Boolean to String

---
+++ TypeConversion/BooleanToString.java
@@ -0,0 +1,14 @@
+public class BooleanToString {
+    public static void main(String[] args) {
+        
+        boolean value1 = true;
+        boolean value2 = false;
+
+        String str1 = String.valueOf(value1);
+        String str2 = String.valueOf(value2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+    }
+}
