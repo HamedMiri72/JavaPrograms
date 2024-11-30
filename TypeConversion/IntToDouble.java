29217fed4b1854ebaf95be80dd3eafbc0420ded0
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-30 22:54:44 +0000

    IntToDouble

---
+++ TypeConversion/IntToDouble.java
@@ -0,0 +1,11 @@
+public class IntToDouble {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        double c = a;
+        double d = b;
+        
+    }
+    
+}
