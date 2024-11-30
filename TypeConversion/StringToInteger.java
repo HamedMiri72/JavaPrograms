cda40345fc660129b955a42cc03d641a02e8f5bd
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-30 22:48:32 +0000

    StringToInteger

---
+++ TypeConversion/StringToInteger.java
@@ -0,0 +1,12 @@
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        String str1 = "2";
+        String str2 = "3";
+
+        int a = Integer.parseInt(str1);
+        int b = Integer.parseInt(str2);
+        
+    }
+    
+}
