92d1cbe95469a9f30d024b81e9c43716c7f45de3
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-30 23:08:45 +0000

    DoubleToInt

---
+++ TypeConversion/DoubleToInt.java
@@ -0,0 +1,13 @@
+public class DoubleToInt {
+    public static void main(String[] args) {
+        
+        double a = 1122.22D;
+        double b = 123.232D;
+
+        int c = (int) a;
+        int d = (int) b;
+
+
+    }
+    
+}
