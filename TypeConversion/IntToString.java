525551b2e6070946e6e43032d61397cbc4b2573f
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-30 22:51:38 +0000

    IntToString

---
+++ TypeConversion/IntToString.java
@@ -0,0 +1,12 @@
+
+public class IntToString {
+    public static void main(String[] args) {
+        
+        int a = 3;
+        int b = 4;
+
+        String str1 = String.valueOf(a);
+        String str2 = String.valueOf(b);
+        
+    }
+}
