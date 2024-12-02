3eb4bc48b46d2c6e044f07005022563e983141f6
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-02 20:51:15 +0000

    SumNaturalNumbers

---
+++ Loop/SumNaturalNumbers.java
@@ -0,0 +1,15 @@
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        int number = 100;
+        int sum = 0;
+
+        for (int i = 0; i <= number; i++) {
+
+            sum += i;
+            
+        }
+
+        System.out.println("the sum of natural number is :" + sum);
+    }
+    
+}
