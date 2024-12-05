2ae2da28fe96d2667e6b6e3ce68c9ea31e10c379
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-05 22:41:19 +0000

    Alphabetr display

---
+++ Loop/AlphabetDisplay.java
@@ -0,0 +1,14 @@
+public class AlphabetDisplay {
+
+    public static void main(String[] args){
+        
+
+        char alphabet;
+
+        for (alphabet = 'A'; alphabet <= 'Z'; alphabet++){
+            System.out.print(alphabet + ",");
+
+        }
+    }
+    
+}
